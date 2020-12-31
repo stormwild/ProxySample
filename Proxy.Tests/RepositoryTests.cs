fc2553755b7b17d96db68030a9680afe63b822ca
Alexander Torrijos <artorrijos@gmail.com>
2020-12-31 17:49:51 +0800

    Updates

---
+++ Proxy.Tests/RepositoryTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProxyDomain;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Proxy.Tests
+{
+    public class RepositoryTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public RepositoryTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Repository_Tests()
+        {
+            // Arrange
+            IRepository<Customer> customerRepository = new Repository<Customer>();
+
+            var customer = new Customer
+            {
+                Id = 1,
+                Name = "Customer 1",
+                Address = "Address 1"
+            };
+
+            // Act
+            customerRepository.Add(customer);
+            var sut = customerRepository.GetAll();
+
+            // Assert
+            Assert.NotNull(sut);
+            Assert.NotEmpty(sut);
+            Assert.IsAssignableFrom<IEnumerable<Customer>>(sut);
+
+            // Act 
+            customer.Address = "Address 1a";
+            customerRepository.Update(customer);
+            var sut2 = customerRepository.GetById(customer.Id);
+
+            Assert.Equal(customer.Address, sut2.Address);
+
+            _output.WriteLine($"Customer Address: {sut2.Address}");
+        }  
+    }
+}
---
+++ ProxyConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ProxyDomain;
+
+namespace ProxyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***\r\nBegin program - no logging\r\n");
+            
+            IRepository<Customer> customerRepository = new Repository<Customer>();
+
+            var customer = new Customer
+            {
+                Id = 1,
+                Name = "Customer 1",
+                Address = "Address 1"
+            };
+            
+            customerRepository.Add(customer);
+            customerRepository.Update(customer);
+            customerRepository.Delete(customer);
+            
+            Console.WriteLine("\r\nEnd program - no logging\r\n***");
+            Console.ReadLine();
+        }
+    }
+}
